e200b9a78c2106a8b4406fda6b8b9130c33c931a
louish10 <louish10@hotmail.co.uk>
2018-08-12 18:22:21 +0100

    Added dismiss button

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -21,11 +21,24 @@ const list = [
 ];
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      list,
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id){
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({list: updatedList});
+  }
+
   render() {
-    const helloWorld = "Welcome to the Road to learn React";
     return (
       <div className="App">
-        {list.map(item =>
+        {this.state.list.map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -33,6 +46,14 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         )}
       </div>
