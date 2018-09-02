e779fc1be6cb5e4ef0ebc4c21268ffdb28b52574
louish10 <louish10@hotmail.co.uk>
2018-09-02 19:45:01 +0100

    On to advanced components

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
       error: null,
+      isLoading: false,
     };
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
@@ -56,7 +57,8 @@ class App extends Component {
        results: {
          ...results,
          [searchKey]: { hits: updatedHits, page}
-       }
+       },
+       isLoading: false,
      })
   }
 
@@ -72,6 +74,7 @@ class App extends Component {
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
+    this.setState({ isLoading: true });
     axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(result => this._isMounted && this.setSearchTopStories(result.data))
       .catch(error => this.setState({ error }));
@@ -106,7 +109,7 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, results, searchKey, error } = this.state;
+    const { searchTerm, results, searchKey, error, isLoading } = this.state;
     const page = (results && results [searchKey] && results[searchKey].page) || 0;
     const list = (
       results &&
@@ -135,16 +138,19 @@ class App extends Component {
         }
 
         <div className="interactions">
-          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
-            More
-          </Button>
+          { isLoading
+            ? <Loading/>
+            :<Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+              More
+            </Button>
+          }
         </div>
       </div>
     );
   }
 }
 
-
+const Loading = () => <div>Loading...</div>;
 
 const Search = ({ value, onChange, onSubmit, children }) =>
     (<form onSubmit={onSubmit}>
